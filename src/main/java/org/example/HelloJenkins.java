eabfd310303cdfa738e039764aea1cf6abdaa2a0
Moses Lemma <moses.lemma91@gmail.com>
2023-03-28 15:11:03 -0400

    Initial commit

---
+++ src/main/java/org/example/HelloJenkins.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println(helloJenkins());
+        System.out.println(helloJenkinsAgain());
+    }
+
+    public static String helloJenkins() {
+        return "Hello Jenkins!";
+    }
+
+    public static String helloJenkinsAgain() {
+        return "Hello Jenkins again!!!";
+    }
+
+}
